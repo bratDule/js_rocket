682f286cee124392de5999bae1bf81eed5bb5c97
bratDule <64259771+bratDule@users.noreply.github.com>
2020-04-27 15:15:41 +0200

    Create explanation.js

---
+++ explanation.js
@@ -0,0 +1,40 @@
+// Objasnjenje
+var favColor = 'blue';
+var myFavColor = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColor = true;
+var dusanObject = {
+	firstName: 'Dusan',
+	lastName : 'Mihailovic',
+	myFavColor: ['blue', 'red', 'green'],
+	yearsAlive: 98,
+	isMale: true
+};
+
+// Function
+function whatIsMyFavColor () {
+
+}
+
+var whatIsMyFavColor = function () {
+	return false;
+	return true;
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log("do Something!!!");
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' + secondname;
+}
+
+// if statment
+if (true){
+	alert('male');
+};
